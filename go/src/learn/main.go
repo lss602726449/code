1dcd951bc255271e87c7a893afa3511147191e51
lss123789 <602726449@qq.com>
2023-03-28 12:26:21 +0800

    3/281

--- go/src/learn/main.go
+++ go/src/learn/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"learn/test"
+)
+
+func add(a int, b int) (int, string) {
+	n := a + b
+	return n, fmt.Sprintf("%d", n)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	n, s := add(1, 2)
+	fmt.Println(n)
+	fmt.Println(s)
+	test.Test()
 }
